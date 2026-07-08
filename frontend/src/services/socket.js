69788f0724df25b6853df5316f8f6647593ffc92
Chu Quoc Thai <24107856@st.phenikaa-uni.edu.vn>
2026-07-08 18:01:18 +0700

    fix:update loi sai duong dan

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // Trỏ về đúng địa chỉ Backend của em (cổng 5000)
-const socket = io("http://localhost:5000");
+const socket = io("https://btl-online-auction.onrender.com");
 
 export default socket;
