b7df69875747b5142ae7cd5c0dfb2174efecb8ba
Chu Quoc Thai <24107856@st.phenikaa-uni.edu.vn>
2026-07-08 17:30:33 +0700

    fix

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,17 +4,15 @@ console.log("DEBUG DB_HOST:", process.env.DB_HOST);
 console.log("DEBUG DB_PORT:", process.env.DB_PORT);
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    ssl: {
-  rejectUnauthorized: false,
-  requestCert: true
-}
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, 
+    minVersion: 'TLSv1.2' 
+  }
 });
 
 pool.getConnection()
