24f40bcbca31a08b585b402f0313ebd4688970fd
Chu Quoc Thai <24107856@st.phenikaa-uni.edu.vn>
2026-07-08 17:11:34 +0700

    fix:ssl

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,7 +8,10 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+  rejectUnauthorized: false
+}
 });
 
 pool.getConnection()
